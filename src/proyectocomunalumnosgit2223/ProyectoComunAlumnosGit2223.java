cb2387c9523993adbd5c8427139233a9ab36693c
Kevin <kevnmm96@gmail.com>
2023-04-19 10:57:54 +0200

    Mi método

--- src/proyectocomunalumnosgit2223/ProyectoComunAlumnosGit2223.java
+++ src/proyectocomunalumnosgit2223/ProyectoComunAlumnosGit2223.java
@@ -15,6 +15,8 @@ public class ProyectoComunAlumnosGit2223 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Método de KEVIN MARTINEZ MARTINEZ");
     }
+    
     
 }
