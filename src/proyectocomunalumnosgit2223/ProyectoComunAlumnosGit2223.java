8fb3babdc255a61a93a01fb32181dbac6fcf6f71
jesus <jesus@PC-Yisus>
2023-04-19 10:57:49 +0200

    Actividad 4.3.4 - Método con el nombre

--- src/proyectocomunalumnosgit2223/ProyectoComunAlumnosGit2223.java
+++ src/proyectocomunalumnosgit2223/ProyectoComunAlumnosGit2223.java
@@ -14,7 +14,9 @@ public class ProyectoComunAlumnosGit2223 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Método de Jesús Cruz");
+        
     }
     
 }
